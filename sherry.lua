0ee05ccb27cb1bfa9423c2973faa5164e8aae82b
Dieter Brehm <inkering@gmail.com>
2022-02-13 13:49:15 -0500

    experimental metadata features added

--- sherry.lua
+++ sherry.lua
@@ -2,12 +2,35 @@
 -- v1.0.0 @inkering
 -- llllllll.co/t/22222
 -- 
+-- Requires 
+-- shairport-sync-metadata-reader
+-- from Github for now.
+-- 
 -- turn any enc to select patch
 -- press k1 or k2 to activate
 -- activating a patch deactivates
 -- the others
-
 UI = require("ui")
+
+-- DATA FORMAT, 18 lines post decode
+-- 1 - "ssnc" "pfls": "3216497574".
+-- 2 - "ssnc" "pffr": "".
+-- 3 - "ssnc" "mdst": "3216497574".
+-- 4 - Album Name: "The Dream".
+-- 5 - Artist: "alt-J".
+-- 6 - Comment: "".
+-- 7 - Composer: "Gus Unger-Hamilton, Joe Newman & Thom Sonny Green".
+-- 8 - Genre: "Alternative".
+-- 9 - File kind: "Apple Music AAC audio file".
+--10 - Title: "The Actor".
+--11 - Persistent ID: "bc374e9feef416a7".
+--12 - Sort as: "Actor".
+--13 - "ssnc" "mden": "3216497574".
+--14 - "ssnc" "pcst": "3216497574".
+--15 - Picture received, length 544616 bytes.
+--16 - "ssnc" "pcen": "3216497574".
+--17 - "ssnc" "prgr": "3216485286/3216497574/3227108682".
+--18 - "ssnc" "prsm": "".
 
 patches = {'input','nothing'}
 
@@ -15,17 +38,32 @@ scroll = {}
 
 scroll[1] = UI.ScrollingList.new(55,8,1,patches)
 
-message = {}
+local message = "K3 to get song title"
+
+-- for now, requiring shairport-sync-metadata-reader
+-- TODO internalize base64 decode
 
 function init()
    -- initialization
    os.execute 'jack_connect shairport-sync:out_L crone:input_1'
    os.execute 'jack_connect shairport-sync:out_R crone:input_2'
+   os.execute 'shairport-sync-metadata-reader < /tmp/shairport-sync-metadata | tee /tmp/output.txt &'
+end
+
+function magiclines(s)
+        if s:sub(-1)~="\n" then s=s.."\n" end
+        return s:gmatch("(.-)\n")
 end
 
 function redraw()
    screen.clear()
    screen.font_size(8)
+   screen.level(15)
+   screen.move(0,8)
+   screen.text("Song")
+   screen.move(0,16)
+   -- the title from shairplay meta tmp file
+   screen.text(message)
    scroll[1]:redraw()
    screen.move(0,34)
    screen.level(15)
@@ -44,13 +82,16 @@ function key(n,z)
     end
   end
     if n == 3 and z == 1 then
-    if scroll[1].index == 1 then
-        os.execute 'jack_connect shairport-sync:out_L crone:input_1'
-        os.execute 'jack_connect shairport-sync:out_R crone:input_2'
-    elseif scroll[1].index == 2 then
-       os.execute 'jack_disconnect shairport-sync:out_L crone:input_1'
-       os.execute 'jack_disconnect shairport-sync:out_R crone:input_2'
-    end
+      local f = assert(io.open('/tmp/output.txt', "r"))
+      local content = f:read("*all")
+      f:close()
+      for line in magiclines(content) do
+        if line:sub(1, 5) == "Title" then
+          message = line 
+        end
+      end
+      -- empty the file when we're done
+      io.open('/tmp/output.txt',"w"):close()
   end
 
   redraw()
@@ -68,5 +109,7 @@ function enc(n,d)
 end
 
 function cleanup()
-  -- deinitialization
+   -- deinitialization
+   -- kill the tracker for the metadata
+   os.execute('pkill -f "shairport-sync-metadata-reader"')
 end
